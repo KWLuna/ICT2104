cdaaed40375e5605ec72cac1404607a7066910bc
Sophie Wong <42300517+sophiewongsp@users.noreply.github.com>
2022-11-23 20:08:18 +0800

    🐛BUGFIX: visited node count fix
    also moved functions below main()

--- 2104_Project/mapping.c
+++ 2104_Project/mapping.c
@@ -101,131 +101,15 @@ Direction GetBackDirection(Direction frontDirection)
 //array used by navigation code
 int navigationArray[9][11];
 
-void MarkWall(int x, int y, Node gridArray[][9], float ultrasonicDistance, Direction ultrasonicDirection)
-{
-    // if ultrasonic value is more than distance, it is not a wall
-    if (ultrasonicDistance > 13.5f) // check how many cm is wall from car (one grid is 27cm)
-    {
-        // mark ultrasonic direction as gap
-        switch(ultrasonicDirection)
-        {
-            case North:
-                gridArray[x][y].northIsWall = 1;
-                break;
-            case East:
-                gridArray[x][y].eastIsWall = 1;
-                break;
-            case South:
-                gridArray[x][y].southIsWall = 1;
-                break;
-            case West:
-                gridArray[x][y].westIsWall = 1;
-                break;
-        }
-    }
-    else    // it is a wall
-    {
-        // mark ultrasonic direction as wall
-        switch(ultrasonicDirection)
-        {
-            case North:
-                gridArray[x][y].northIsWall = 0;
-                break;
-            case East:
-                gridArray[x][y].eastIsWall = 0;
-                break;
-            case South:
-                gridArray[x][y].southIsWall = 0;
-                break;
-            case West:
-                gridArray[x][y].westIsWall = 0;
-                break;
-        }
-    }
-}
+// global so that its values are 0 at initialisation
+Grid grid;  // The Map 
+int numNodeVisited = 0; // counter for number of nodes visited
 
-// checks current node's walls
-void CheckNode(int x, int y, Node gridArray[][9], Direction directionFacing)
-{
-    // if current node has not been visted
-    if (gridArray[x][y].isVisited == 0)
-    {
-        // set node to visited
-        gridArray[x][y].isVisited = 1;
-
-        // check front, left, right sides for wall
-        float frontUltrasonicDist = 2.f; // call function to get front ultrasonic distance <to be done>
-        float leftUltrasonicDist = 20.f; // call function to get left ultrasonic distance <to be done>
-        float rightUltrasonicDist = 20.f; // call function to get right ultrasonic distance <to be done>
-
-        MarkWall(x, y, gridArray, frontUltrasonicDist, directionFacing);
-        MarkWall(x, y, gridArray, leftUltrasonicDist, GetLeftDirection(directionFacing));
-        MarkWall(x, y, gridArray, rightUltrasonicDist, GetRightDirection(directionFacing));
-
-        // set wall that car came from to isGap
-        MarkWall(x, y, gridArray, 100.f, GetBackDirection(directionFacing));
-    }
-}
-
-void SetCar(Car *car, int xPos, int yPos, Direction direction)
-{
-    car->xCoord = xPos;
-    car->yCoord = yPos;
-    car->directionFacing = direction;
-}
-
-//convert 4x5 into 9x11 
-//conversion guide: (lengthx2) + 1
-void conversionConstructor(Node gridArray[4][5])
-{
-    //initialization node centers
-    //0 = wall, 1 = space
-    
-    for(int x=0; x<9;x++)
-    {
-        for(int y=0; y<11; y++)
-        {
-            navigationArray[x][y]=0;
-        }
-    }
-
-    //constructing navigation array map
-    for(int i=0; i<4; i++)
-    {
-        for(int j=0; j<5; j++)
-        {
-            int refRow = (i*2)+1;
-            int refCol = (j*2)+1;
-
-            //blocking out diagonals for each node
-            navigationArray[refRow-1][refCol-1]=1;
-            navigationArray[refRow-1][refCol+1]=1;
-            navigationArray[refRow+1][refCol-1]=1;
-            navigationArray[refRow+1][refCol+1]=1;
-
-            //creating blocked off walls
-            if(gridArray[i][j].northIsWall == 0)
-            {
-               navigationArray[refRow-1][refCol]=1;
-            }
-            if(gridArray[i][j].southIsWall == 0)
-            {
-               navigationArray[refRow+1][refCol]=1;
-            }
-            if(gridArray[i][j].eastIsWall == 0)
-            {
-               navigationArray[refRow][refCol-1]=1;
-            }
-            if(gridArray[i][j].westIsWall == 0)
-            {
-               navigationArray[refRow][refCol+1]=1;
-            }
-        }
-    }
-}
-
-// The Map
-Grid grid;  // map is global so that its values are 0 at initialisation
+// declare functions
+void MarkWall(int x, int y, Node gridArray[][9], float ultrasonicDistance, Direction ultrasonicDirection);
+void CheckNode(int x, int y, Node gridArray[][9], Direction directionFacing);
+void SetCar(Car *car, int xPos, int yPos, Direction direction);
+void conversionConstructor(Node gridArray[4][5]);
 
 int main()
 {
@@ -235,14 +119,9 @@ int main()
     //Set Car Starting Position
     SetCar(&car, 3, 4, North);
 
-    // counter for number of nodes visited
-    int numNodeVisited = 0;
-
     while (numNodeVisited < 20)
     {
         CheckNode(car.xCoord, car.yCoord, grid.gridArray, car.directionFacing); // mark current node
-
-        numNodeVisited++; // add 1 to number of nodes visited
         
         Node *node = &grid.gridArray[car.xCoord][car.yCoord];   // pointer of current node
 
@@ -395,4 +274,130 @@ int main()
     }
 
     return 0;
+}
+
+void MarkWall(int x, int y, Node gridArray[][9], float ultrasonicDistance, Direction ultrasonicDirection)
+{
+    // if ultrasonic value is more than distance, it is not a wall
+    if (ultrasonicDistance > 13.5f) // check how many cm is wall from car (one grid is 27cm)
+    {
+        // mark ultrasonic direction as gap
+        switch(ultrasonicDirection)
+        {
+            case North:
+                gridArray[x][y].northIsWall = 1;
+                break;
+            case East:
+                gridArray[x][y].eastIsWall = 1;
+                break;
+            case South:
+                gridArray[x][y].southIsWall = 1;
+                break;
+            case West:
+                gridArray[x][y].westIsWall = 1;
+                break;
+        }
+    }
+    else    // it is a wall
+    {
+        // mark ultrasonic direction as wall
+        switch(ultrasonicDirection)
+        {
+            case North:
+                gridArray[x][y].northIsWall = 0;
+                break;
+            case East:
+                gridArray[x][y].eastIsWall = 0;
+                break;
+            case South:
+                gridArray[x][y].southIsWall = 0;
+                break;
+            case West:
+                gridArray[x][y].westIsWall = 0;
+                break;
+        }
+    }
+}
+
+// checks current node's walls
+void CheckNode(int x, int y, Node gridArray[][9], Direction directionFacing)
+{
+    // if current node has not been visted
+    if (gridArray[x][y].isVisited == 0)
+    {
+        // add 1 to number of nodes visited
+        numNodeVisited++;
+
+        // set node to visited
+        gridArray[x][y].isVisited = 1;
+
+        // check front, left, right sides for wall
+        float frontUltrasonicDist = 2.f; // call function to get front ultrasonic distance <to be done>
+        float leftUltrasonicDist = 20.f; // call function to get left ultrasonic distance <to be done>
+        float rightUltrasonicDist = 20.f; // call function to get right ultrasonic distance <to be done>
+
+        MarkWall(x, y, gridArray, frontUltrasonicDist, directionFacing);
+        MarkWall(x, y, gridArray, leftUltrasonicDist, GetLeftDirection(directionFacing));
+        MarkWall(x, y, gridArray, rightUltrasonicDist, GetRightDirection(directionFacing));
+
+        // set wall that car came from to isGap
+        MarkWall(x, y, gridArray, 100.f, GetBackDirection(directionFacing));
+    }
+}
+
+void SetCar(Car *car, int xPos, int yPos, Direction direction)
+{
+    car->xCoord = xPos;
+    car->yCoord = yPos;
+    car->directionFacing = direction;
+}
+
+//convert 4x5 into 9x11 
+//conversion guide: (lengthx2) + 1
+void conversionConstructor(Node gridArray[4][5])
+{
+    //initialization node centers
+    //0 = wall, 1 = space
+    
+    for(int x=0; x<9;x++)
+    {
+        for(int y=0; y<11; y++)
+        {
+            navigationArray[x][y]=0;
+        }
+    }
+
+    //constructing navigation array map
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            int refRow = (i*2)+1;
+            int refCol = (j*2)+1;
+
+            //blocking out diagonals for each node
+            navigationArray[refRow-1][refCol-1]=1;
+            navigationArray[refRow-1][refCol+1]=1;
+            navigationArray[refRow+1][refCol-1]=1;
+            navigationArray[refRow+1][refCol+1]=1;
+
+            //creating blocked off walls
+            if(gridArray[i][j].northIsWall == 0)
+            {
+               navigationArray[refRow-1][refCol]=1;
+            }
+            if(gridArray[i][j].southIsWall == 0)
+            {
+               navigationArray[refRow+1][refCol]=1;
+            }
+            if(gridArray[i][j].eastIsWall == 0)
+            {
+               navigationArray[refRow][refCol-1]=1;
+            }
+            if(gridArray[i][j].westIsWall == 0)
+            {
+               navigationArray[refRow][refCol+1]=1;
+            }
+        }
+    }
 }
