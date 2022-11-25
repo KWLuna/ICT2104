8015c46d3e870db0fec443392bfa8a82587ba158
Sophie Wong <42300517+sophiewongsp@users.noreply.github.com>
2022-11-25 20:17:28 +0800

    minor changes

--- 2104_Project/mapping.h
+++ 2104_Project/mapping.h
@@ -9,8 +9,8 @@
 #define NUMBER_OF_NODES 20
 #define MAPPING_GRID_SIZE_X 7
 #define MAPPING_GRID_SIZE_Y 9
-#define CAR_START_X 2
-#define CAR_START_Y 0
+#define CAR_START_X 3
+#define CAR_START_Y 4
 
 // Node is used by Grid
 typedef struct Node
