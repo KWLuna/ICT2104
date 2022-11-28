e1c9f1120d829cd8776865b80b5b51e942b71db5
baohuan <92810012+bhuann@users.noreply.github.com>
2022-11-28 21:17:11 +0800

    Update encoder.c

--- IR & Encoder/encoder.c
+++ IR & Encoder/encoder.c
@@ -15,6 +15,35 @@ volatile int current_notches = 0; // For Moving by distance
 volatile int rightNotches = 0;  // For Turning
 volatile int leftNotches = 0;
 
+void init_pins()
+{
+    stdio_init_all();
+
+    // LED initialise
+    gpio_init(LED_PIN); 
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    //encoder
+    gpio_init(RIGHT_ENCODER_PIN);
+    gpio_set_dir(RIGHT_ENCODER_PIN, GPIO_IN);
+    gpio_pull_up(RIGHT_ENCODER_PIN);
+
+    gpio_init(LEFT_ENCODER_PIN);
+    gpio_set_dir(LEFT_ENCODER_PIN, GPIO_IN);
+    gpio_pull_up(LEFT_ENCODER_PIN);
+}
+
+//Timer ISR
+bool encoder_timer(struct repeating_timer *t){  
+    int rightRotation = (rightCounter / stepCount) * 60.00; //calculate RPM for right motor
+    rightCounter = 0; //reset counter to 0
+
+    int leftRotation = (leftCounter / stepCount) * 60.00;
+    leftCounter = 0; //reset counter to 0
+    printf("left RPM: %d\n", leftRotation);
+    return true;
+}
+
 //right encoder pulse count ISR
 void ISR_rightEncoder(){
     rightCounter++;
@@ -79,34 +108,4 @@ void gpio_callback(uint gpio, uint32_t events){
         ISR_leftEncoder();
         //printf("right: %d", leftCounter);
     }
-}
-
-void init_pins()
-{
-    stdio_init_all();
-
-    // LED initialise
-    gpio_init(LED_PIN); 
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    //encoder
-    gpio_init(RIGHT_ENCODER_PIN);
-    gpio_set_dir(RIGHT_ENCODER_PIN, GPIO_IN);
-    gpio_pull_up(RIGHT_ENCODER_PIN);
-
-    gpio_init(LEFT_ENCODER_PIN);
-    gpio_set_dir(LEFT_ENCODER_PIN, GPIO_IN);
-    gpio_pull_up(LEFT_ENCODER_PIN);
-}
-
-//Timer ISR
-bool encoder_timer(struct repeating_timer *t){  
-    int rightRotation = (rightCounter / stepCount) * 60.00; //calculate RPM for right motor
-    rightCounter = 0; //reset counter to 0
-
-    int leftRotation = (leftCounter / stepCount) * 60.00;
-    leftCounter = 0; //reset counter to 0
-    printf("left RPM: %d\n", leftRotation);
-    return true;
-
 }
